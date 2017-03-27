aa2f78c19bb6a214b24836bc0f5f31688041d226
appcreate35 <appcreate35@appcreate35ui-iMac.local>
2017-03-27 19:08:18 +0900

    #2 java Event comlete

--- app/src/main/java/woojy/kr/hs/emirim/edittexttest1/MainActivity.java
+++ app/src/main/java/woojy/kr/hs/emirim/edittexttest1/MainActivity.java
@@ -1,13 +1,32 @@
 package woojy.kr.hs.emirim.edittexttest1;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import static woojy.kr.hs.emirim.edittexttest1.R.id.edit1;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edit1;
+    TextView textResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edit1 = (EditText)findViewById(R.id.edit1);
+        Button butOk=(Button)findViewById(R.id.but_okay);
+        textResult = (TextView)findViewById(R.id.text_result);
+
+        butOk.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                String name=edit1.getText().toString();
+                textResult.setText("입력한 성명은 "+name+"입니다");
+            }
+        });
     }
 }
